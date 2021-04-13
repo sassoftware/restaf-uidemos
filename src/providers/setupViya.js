745b7c5a1777f7c6b475d8285e1ace8e7dcc4ecd
Deva Kumar <deva.kumar@sas.com>
2021-04-13 16:37:26 -0400

    updated dockerfile and dockercompose

--- src/providers/setupViya.js
+++ src/providers/setupViya.js
@@ -7,9 +7,10 @@ import { initStore } from '@sassoftware/restaf/dist/restaf.js';
 import { lib } from '@sassoftware/restaflib/dist/restaflib.js';
 async function setupViya() {
 	let store = initStore();
+	debugger;
 	await store.logon(window.appOptions.logonPayload);
 	/* Commonly used services - can remove it and do it on demand as many times as needed*/
-	await store.addServices('casManagement', 'compute');
+	await store.addServices('files');
 	let r = await fetch('./README.md');
 	let text = await r.text();
 	let appOptions = { ...window.appOptions };
