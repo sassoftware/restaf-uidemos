909b6d7963597fb2b6e56441d6077c5cf9671387
Deva Kumar <deva.kumar@sas.com>
2022-06-29 13:06:58 -0400

    save

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
+/* eslint-disable prettier/prettier */
 /*
  * Copyright © 2019, SAS Institute Inc., Cary, NC, USA.  All Rights Reserved.
  * SPDX-License-Identifier: Apache-2.0
  */
+
 import DataEditor from './components/DataEditor.js';
 import DataEditorVisual from './components/DataEditorVisual.js';
-// let lib = require('./lib');
+// import lib from './lib';
 
-export {
-    DataEditor,
-    DataEditorVisual
-}
+export { DataEditor, DataEditorVisual}
