6352699e5acec4346df3876a74d6c8fccebe299f
Deva Kumar <deva.kumar@sas.com>
2022-06-29 14:36:36 -0400

    save

---
+++ rollup.config.js
@@ -0,0 +1,39 @@
+import babel from 'rollup-plugin-babel'
+import commonjs from 'rollup-plugin-commonjs'
+import postcss from 'rollup-plugin-postcss'
+import resolve from 'rollup-plugin-node-resolve'
+
+import pkg from './package.json'
+
+export default {
+  input: 'src/index.js',
+  output: [
+    {
+      file: pkg.main,
+      format: 'cjs',
+      exports: 'named'
+    },
+    {
+      file: pkg.module,
+      format: 'es',
+      exports: 'named'
+    }
+  ],
+  external: [
+    'react',
+    'react-dom',
+    'prop-types',
+    'restaf',
+    'restaflib'
+  ],
+  plugins: [
+    postcss({
+      modules: true
+    }),
+    babel({
+      exclude: 'node_modules/**'
+    }),
+    resolve(),
+    commonjs()
+  ]
+}
