6c3befd1d9ec9636a5a4cce8fd12355696618bd0
Deva Kumar <deva.kumar@sas.com>
2022-07-05 12:55:23 -0400

    ave

--- src/lib/text2Float.js
+++ src/lib/text2Float.js
@@ -6,6 +6,9 @@ function text2Float (value, f) {
     let svalue = value;
     if (typeof svalue === 'string' && (f.Type === 'decimal' || f.Type === 'number' || f.Type === 'double')) {
         svalue = parseFloat(value * 1.0);
+        if (isNan(value) === true) {
+            value = 0;
+        }
     }
     return svalue;
 }
