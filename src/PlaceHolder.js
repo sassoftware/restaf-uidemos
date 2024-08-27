f96e49574a2fc6235c208a303285ad5cf633b40e
Deva <deva.kumar@sas.com>
2024-08-27 15:14:47 -0400

    set zIndex for placeholder

--- src/PlaceHolder.js
+++ src/PlaceHolder.js
@@ -2,11 +2,10 @@
  * Copyright © 2021, SAS Institute Inc., Cary, NC, USA.  All Rights Reserved.
  * SPDX-License-Identifier: Apache-2.0
  */
-
 import React  from 'react';
 function PlaceHolder (props) {
   const {sx} = props;
-  let isx = {height: 'inherit', zindex: -1, borderStyle: 'solid',borderWidth: '1px', borderRadius: 8, width: 'inherit', ...sx};
-  return <div style={isx}>{props.value}</div>
+  let isx = {height: 'inherit',  zIndex: 1, borderStyle: 'solid',borderWidth: '1px', borderRadius: 8, width: 'inherit', ...sx};
+  return <div style={isx} >{props.value}</div>
 }
 export default PlaceHolder;
