e8b3974ab0e546e98d4e86bafbef76263e3a378d
Deva <deva.kumar@sas.com>
2024-11-12 21:15:15 -0500

    save

--- webpack.config.js
+++ webpack.config.js
@@ -28,7 +28,7 @@ module.exports = (env) =>  {
     optimization: optimize,
     output: {
       path: usePath,
-      filename: (env.p === 'y') ? 'index.js' : 'index.dev.js',
+      filename: (env.p === 'y') ? 'index.min.js' : 'index.umd.js',
       libraryTarget: 'umd',
       library: "smartControls", 
       umdNamedDefine: true,
