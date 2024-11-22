29403a6efb8d28953a9888f41ab2bed79f73cbc6
Deva <deva.kumar@sas.com>
2024-11-22 12:43:54 -0500

    save

--- src/DataGrid.js
+++ src/DataGrid.js
@@ -48,6 +48,7 @@ function DataGrid(props) {
         let r = await setup(viyaEnv.logonPayload, appControl, viyaEnv.sessionID);
         debugger;
         await scrollTable('first', r);
+        debugger;
         let cols = [];
         for (let k in r.state.columns) {
           let c = r.state.columns[k];
@@ -61,6 +62,8 @@ function DataGrid(props) {
             });
           }
         };
+        console.log(r.state.data[0]);
+        console.log(cols);
         setColumns(cols);
         setAppEnv(r);
         debugger;
@@ -85,11 +88,11 @@ function DataGrid(props) {
   }, [value, lib, name, _userProps]);
   console.log(scroll);
   let style = { height: 'inherit', width: 'inherit', borderStyle: 'solid', borderRadius: 2, borderWidth: '1px', borderColor: 'black', ...sx };
-  let eProps = { pageSize: 20, rowsPerPageOptions: [20], checkboxSelection: false, border: 1, disableSelectionOnClick: true, ...rest };
+  let eProps = { autoPageSize: true, checkboxSelection: false, border: 1, disableSelectionOnClick: true, ...rest };
 
   let show = <Box style={style}>
     {appEnv === null ? <p> No data to display </p> :
-      <ViyaDataGrid rows={appEnv === null ? null : appEnv.state.rows} columns={columns} {...eProps} style={style} />}
+      <ViyaDataGrid rows={appEnv === null ? null : appEnv.state.data} columns={columns} {...eProps} style={style} />}
   </Box>
  
   return show;
