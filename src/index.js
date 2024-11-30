ee056eccc81cf62337adc78192a677d2e9401e17
Deva <deva.kumar@sas.com>
2024-11-30 13:54:22 -0500

    include reactdom?

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@
  * Copyright © 2024, SAS Institute Inc., Cary, NC, USA.  All Rights Reserved.
  * SPDX-License-Identifier: Apache-2.0
  */
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 import Checkbox from './Checkbox';
 import Input from './Input';
 import InputNumber from './InputNumber';
