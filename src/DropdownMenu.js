8a04fe64955b16590aedeba5e61a6b0e4a11fc25
Deva <deva.kumar@sas.com>
2025-01-16 14:26:45 -0500

    fix dropdownmenu to return an array

--- src/DropdownMenu.js
+++ src/DropdownMenu.js
@@ -9,7 +9,7 @@ import MoreVert from '@mui/icons-material/MoreVert';
 function DropDownMenu(props) {
   let { items, sx, valueType, label,_appContext, ...eProps } = props;
   const onSelect = (m, i) => {
-    props.onChange(valueType === 'index' ? i : m);
+    props.onChange(valueType === 'index' ? [i] : [m]);
   };
   
   let menuList = [];
